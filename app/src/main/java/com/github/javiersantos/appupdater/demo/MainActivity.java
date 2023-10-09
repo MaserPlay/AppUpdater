42bd3191e0eb93655f077071143fe60e4fc9389a
Dmitriy <super.dima771@yandex.ru>
2023-10-10 00:08:08 +0300

    Initial commit

---
+++ app/src/main/java/com/github/javiersantos/appupdater/demo/MainActivity.java
@@ -0,0 +1,143 @@
+package com.github.javiersantos.appupdater.demo;
+
+import android.content.Context;
+import android.content.Intent;
+import androidx.databinding.DataBindingUtil;
+import android.net.Uri;
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+
+import com.github.javiersantos.appupdater.AppUpdater;
+import com.github.javiersantos.appupdater.R;
+import com.github.javiersantos.appupdater.databinding.ActivityMainBinding;
+import com.github.javiersantos.appupdater.enums.Display;
+import com.github.javiersantos.appupdater.enums.UpdateFrom;
+
+public class MainActivity extends AppCompatActivity {
+    private Context mContext;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        ActivityMainBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        this.mContext = this;
+        setSupportActionBar(binding.toolbar);
+        binding.fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/javiersantos/AppUpdater")));
+            }
+        });
+
+        binding.included.dialogUpdateChangelog.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new AppUpdater(mContext)
+                        //.setUpdateFrom(UpdateFrom.GITHUB)
+                        //.setGitHubUserAndRepo("javiersantos", "AppUpdater")
+                        .setUpdateFrom(UpdateFrom.JSON)
+                        .setUpdateJSON("https://raw.githubusercontent.com/javiersantos/AppUpdater/master/app/update-changelog.json")
+                        .setDisplay(Display.DIALOG)
+                        .showAppUpdated(true)
+                        .start();
+            }
+        });
+
+        binding.included.dialogUpdate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new AppUpdater(mContext)
+                        //.setUpdateFrom(UpdateFrom.GITHUB)
+                        //.setGitHubUserAndRepo("javiersantos", "AppUpdater")
+                        .setUpdateFrom(UpdateFrom.JSON)
+                        .setUpdateXML("https://raw.githubusercontent.com/javiersantos/AppUpdater/master/app/update.json")
+                        .setDisplay(Display.DIALOG)
+                        .showAppUpdated(true)
+                        .start();
+            }
+        });
+
+        binding.included.snackbarUpdate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new AppUpdater(mContext)
+                        //.setUpdateFrom(UpdateFrom.GITHUB)
+                        //.setGitHubUserAndRepo("javiersantos", "AppUpdater")
+                        .setUpdateFrom(UpdateFrom.XML)
+                        .setUpdateXML("https://raw.githubusercontent.com/javiersantos/AppUpdater/master/app/update.xml")
+                        .setDisplay(Display.SNACKBAR)
+                        .showAppUpdated(true)
+                        .start();
+            }
+        });
+
+        binding.included.notificationUpdate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new AppUpdater(mContext)
+                        //.setUpdateFrom(UpdateFrom.GITHUB)
+                        //.setGitHubUserAndRepo("javiersantos", "AppUpdater")
+                        .setUpdateFrom(UpdateFrom.XML)
+                        .setUpdateXML("https://raw.githubusercontent.com/javiersantos/AppUpdater/master/app/update.xml")
+                        .setDisplay(Display.NOTIFICATION)
+                        .showAppUpdated(true)
+                        .start();
+            }
+        });
+
+        binding.included.dialogNoUpdate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new AppUpdater(mContext)
+                        .setUpdateFrom(UpdateFrom.GOOGLE_PLAY)
+                        .setDisplay(Display.DIALOG)
+                        .showAppUpdated(true)
+                        .start();
+            }
+        });
+
+        binding.included.snackbarNoUpdate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new AppUpdater(mContext)
+                        .setUpdateFrom(UpdateFrom.GOOGLE_PLAY)
+                        .setDisplay(Display.SNACKBAR)
+                        .showAppUpdated(true)
+                        .start();
+            }
+        });
+
+        binding.included.notificationNoUpdate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new AppUpdater(mContext)
+                        .setUpdateFrom(UpdateFrom.GOOGLE_PLAY)
+                        .setDisplay(Display.NOTIFICATION)
+                        .showAppUpdated(true)
+                        .start();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.action_settings) {
+            startActivity(new Intent(this, SettingsActivity.class));
+            return true;
+        } else {
+            return super.onOptionsItemSelected(item);
+        }
+    }
+}
